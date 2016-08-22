a76596befbaffa96dc1706311b87e73567b8bfe8
Dima <magicmind25@gmail.com>
2016-08-22 21:59:23 +0300

    + point class + draw function moved to point class + rewrite program using point class

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 namespace Snake
 {
-	public class Point
+	class Point
 	{
-		public Point()
-		{
+		public int x;
+		public int y;
+		public char sym;
+
+		public void Draw()
+		{ 
+			Console.SetCursorPosition( x, y );
+			Console.Write( sym );
 		}
 	}
 }
