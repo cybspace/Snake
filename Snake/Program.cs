921e0eff8b53a4fa7b37302f19cbb0a3f9c4d49b
Dima <magicmind25@gmail.com>
2016-10-30 10:13:29 +0300

    +moving snake incapsulation

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -24,30 +24,17 @@ namespace Snake
 			Point p = new Point(4, 6, '*');
 			Snake snake = new Snake(p, 4, Direction.RIGHT);
 			snake.Draw();
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
+
+			while (true) 
+			{
+				if (Console.KeyAvailable) 
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+				Thread.Sleep(100);
+				snake.Move();
+			}
 
 			Console.ReadLine();
 
